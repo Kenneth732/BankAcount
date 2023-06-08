8a8e69d92a27ac66c3e452eb7bcc529a0ece2bbe
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-08 10:58:24 +0300

    BankAccount proto... constructor func

--- App.js
+++ App.js
@@ -2,3 +2,8 @@ function BankAccount(initialDeposit) {
     this.balance = initialDeposit;
   }
   
+  BankAccount.prototype.deposit = function(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+    }
+  };
